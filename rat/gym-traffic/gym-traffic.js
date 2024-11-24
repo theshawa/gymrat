b5720d2b917fd33ec4ce5b1be949ef0c4d89bd54
theshawa <mrclocktd@gmail.com>
2024-11-25 00:17:14 +0530

    live traffic ui

---
+++ rat/gym-traffic/gym-traffic.js
@@ -0,0 +1,66 @@
+const meter = document.querySelector(".meter");
+const arrow = document.querySelector(".meter .arrow");
+const textIncrementer = document.querySelector("h2");
+
+const animateMeter = (targetValue = 0) => {
+  let currentValue = 0;
+  const maxValue = 10;
+
+  const animate = () => {
+    if (currentValue <= targetValue) {
+      const rotation = (currentValue / maxValue) * 180;
+      arrow.style.transform = `translateX(-50%) rotate(${rotation}deg)`;
+
+      let color;
+      if (currentValue < maxValue / 3) {
+        color = `rgb(66, 179, 77)`;
+      } else if (currentValue < (2 * maxValue) / 3) {
+        color = `rgb(255, 204, 0)`;
+      } else {
+        color = `rgb(240, 80, 80)`;
+      }
+
+      meter.style.backgroundColor = color;
+      meter.style.borderColor = color;
+      arrow.style.backgroundColor = color;
+      document.querySelector(".data .active-users .dot").style.backgroundColor =
+        color;
+
+      currentValue += 0.1;
+      requestAnimationFrame(animate);
+    }
+  };
+
+  animate();
+};
+
+let currentIncrement = 0;
+const incrementCount = () => {
+  const intervalSpeed = 1200 / $TRAFFIC_MEASURE / 9;
+  const interval = setInterval(() => {
+    if (currentIncrement < $TRAFFIC_MEASURE) {
+      currentIncrement += 0.1;
+      textIncrementer.innerText = `${currentIncrement.toFixed(1)}/10`;
+    } else {
+      clearInterval(interval);
+      textIncrementer.innerText = `${$TRAFFIC_MEASURE.toFixed(1)}/10`;
+    }
+  }, intervalSpeed);
+};
+
+if ($TRAFFIC_MEASURE < 3) {
+  animateMeter(10);
+  setTimeout(() => {
+    animateMeter($TRAFFIC_MEASURE);
+    incrementCount();
+    setTimeout(() => {
+      document.querySelector(".data").style.opacity = "1";
+    }, 1500);
+  }, 2500);
+} else {
+  animateMeter($TRAFFIC_MEASURE);
+  setTimeout(incrementCount, 500);
+  setTimeout(() => {
+    document.querySelector(".data").style.opacity = "1";
+  }, 1500);
+}
--- rat/gym-traffic/index.php
+++ rat/gym-traffic/index.php
@@ -1,19 +1,65 @@
 <?php
 $pageConfig = [
     "title" => "Gym Traffic",
+    "styles" => ["./gym-traffic.css"],
+    "scripts" => ["./gym-traffic.js"],
     "titlebar" => [
         "title" => "Gym Traffic",
-        "back_url" => "../"
+        "back_url" => "../",
     ],
     "need_auth" => true
 ];
+
+$active_users = 72;
+$traffic_measure = $active_users / 83 * 10;
+
+$status_list = [
+    "Gym is all yours",
+    "Gym is buzzing",
+    "Gym is packed",
+];
+
+$status = $traffic_measure > 0.66 * 10 ? $status_list[2] : ($traffic_measure > 0.33 * 10 ? $status_list[1] : $status_list[0]);
 
 require_once "../includes/header.php";
 require_once "../includes/titlebar.php";
 ?>
 
+<script>
+    const $TRAFFIC_MEASURE = <?= $traffic_measure ?>;
+</script>
+
 <main>
-    <p>Gym Traffic</p>
+    <?php
+    // $subnavbarConfig = [
+    //     'links' => [
+    //         [
+    //             'title' => 'Live',
+    //             'href' => './'
+    //         ],
+    //         [
+    //             'title' => 'Past Week',
+    //             'href' => './past-week'
+    //         ]
+    //     ],
+    //     "active" => 1
+    // ];
+
+    // require_once "../includes/subnavbar.php"; 
+    ?>
+    <div class="meter">
+        <div class="arrow"></div>
+    </div>
+    <span class="label">Live Traffic</span>
+    <h2>0/10</h2>
+    <div class="data">
+        <h1><?= $status ?></h1>
+        <div class="active-users">
+            <div class="dot"></div>
+            <span><?= $active_users ?> rat<?= $active_users === 1 ? " is" : "s are" ?> working out</span>
+        </div>
+        <p class="paragraph small">*The traffic values are estimates to give you a general idea and may not reflect exact conditions. Use them as a guide, and remember—you can crush your workout at any time!</p>
+    </div>
 </main>
 
 <?php require_once "../includes/navbar.php" ?>

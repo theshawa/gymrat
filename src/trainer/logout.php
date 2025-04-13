7effbea614d3aa8c93511a0d8201f7e04118ee01
rowneth <wadroneth@gmail.com>
2025-04-13 16:16:37 +0530

    trainer bugs fixed

--- src/trainer/logout.php
+++ src/trainer/logout.php
@@ -1,5 +1,28 @@
 <?php
+// Start the session before trying to destroy it
+session_start();
 
+// Clear all session data
+$_SESSION = array();
+
+// Delete the session cookie if it exists
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
+    );
+}
+
+// Destroy the session
 session_destroy();
 
+// Redirect to login page
 header("Location: ./login");
+exit();
+?>
